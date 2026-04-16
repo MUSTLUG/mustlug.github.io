50d7438d5f3998e14025463190f48b4ee765f262
Saurlax <saurlax@qq.com>
2026-04-16 15:48:23 +0800

    feat: update to astro v6

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,16 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const events = defineCollection({
+  loader: glob({ base: "./src/content/events", pattern: "**/*.md" }),
   schema: z.object({
     title: z.string(),
     date: z.date(),
   }),
 });
 const news = defineCollection({
+  loader: glob({ base: "./src/content/news", pattern: "**/*.md" }),
   schema: z.object({
     title: z.string(),
     date: z.date(),
